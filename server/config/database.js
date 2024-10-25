2e03f157d4da02408be4be0900d4a26514f37629
Ansh Khandelwal <ansh@Anshs-MacBook-Pro.local>
2024-10-24 17:46:06 -0700

    set up database and seeding

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,14 @@
 /* Establish the DB connection pool here. */
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.CONNECTION_STRING);
 const config = {
+    connectionString: process.env.CONNECTION_STRING
 };
-export const pool = new pg.Pool(config);
+
+const pool = new pg.Pool(config);
+
+export { pool };
--- server/config/seed-db.js
+++ server/config/seed-db.js
@@ -14,6 +14,16 @@ const dropTables = async () => {
 
 const createTables = async () => {
     try {
+        const createTablesQuery = `
+            CREATE TABLE IF NOT EXISTS restaurants (
+                id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                name TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                address TEXT NOT NULL,
+                photo TEXT NOT NULL
+            );
+        `;
+        await pool.query(createTablesQuery);
     } catch (error) {
         console.log(error)
     }
@@ -21,6 +31,17 @@ const createTables = async () => {
 
 const insertData = async () => {
     try {
+        const insertQuery = `
+        INSERT INTO restaurants (name, phone, address, photo)
+        VALUES
+            ('Tofuya-Ukai', '03-3436-1028', '4 Chome-4-13 Shibakoen, Minato City, Tokyo 105-0011, Japan', 'images/tofuya-ukai.JPG'),
+            ('Sushi Dai', '81 3-6633-0042', '6 Chome-5-1 Toyosu, Koto City, Tokyo 135-0061, Japan', 'images/SushiDai.jpg'),
+            ('Kawashima Tōfu', '81 955-72-2423', '1775 Kyomachi, Karatsu, Saga 847-0045, Japan', 'images/KawashimaTōfu.jpg'),
+            ('Menya Saimi', '81 11-820-6511', '5 Chome-3-12 Misono 10 Jo, Toyohira Ward, Sapporo, Hokkaido 062-0010, Japan', 'images/MenyaSaimi.jpg'),
+            ('Asakusa Imahan', '81 3-3841-1114', '3 Chome-1-12 Nishiasakusa, Taito City, Tokyo 111-0035, Japan', 'images/AsakusaImahan.jpg'),
+            ('Kozue', '03-5323-3460', '40th fl, Park Hyatt Tokyo, 3-7-1-2 Nishi-Shinjuku, Shinjuku-ku', 'images/Kozue.jpg');
+        `;
+        await pool.query(insertQuery);
     } catch (error) {
         console.log(error)
     }
