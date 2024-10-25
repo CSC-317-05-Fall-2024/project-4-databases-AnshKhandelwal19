8b85588ed25aeda4aec64678e56e26c776b6a19b
Ansh Khandelwal <ansh@Anshs-MacBook-Pro.local>
2024-10-24 17:57:15 -0700

    added working insert for database into post req

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createRestaurant, deleteRestaurant, editRestaurant } from '../data/restaurants.js'
+import { pool } from '../config/database.js'
 
 const router = express.Router();
 
@@ -9,6 +10,15 @@ router.post('/restaurants', (req, res) => {
     const newRestaurant = req.body;
     try{
         const restuarant = createRestaurant(newRestaurant);
+
+        //Add restaurant to database
+
+        const insertQuery = `
+        INSERT INTO restaurants (name, phone, address, photo)
+        VALUES
+            ('${restuarant.name}', '${restuarant.phone}', '${restuarant.address}', '${restuarant.photo}')
+        `;
+        pool.query(insertQuery);
         res.status(200).json(restuarant);
     }catch(error) {
         res.status(500).json({"message": `${error}`});
